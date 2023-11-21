b3f59983c1c55a3c27fc3f48002041280c3eaeba
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2023-11-22 01:34:08 +0530

    Header Updated

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,7 +1,8 @@
-import { useSelector } from 'react-redux';
-import { Outlet, Navigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
 
 export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
+
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
