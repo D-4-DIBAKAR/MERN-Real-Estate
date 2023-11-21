f32816403cf66e43d90b227fa736e73c77ced2c5
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2023-11-21 22:50:51 +0530

    Minor changes committed

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,17 +1,11 @@
-import bcryptjs from 'bcryptjs';
-import User from '../models/user.model.js';
-import { errorHandler } from '../utils/error.js';
-import Listing from '../models/listing.model.js';
-
-export const test = (req, res) => {
-  res.json({
-    message: 'Api route is working!',
-  });
-};
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only update your own account!'));
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -40,11 +34,11 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only delete your own account!'));
+    return next(errorHandler(401, "You can only delete your own account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
-    res.status(200).json('User has been deleted!');
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
@@ -59,19 +53,18 @@ export const getUserListings = async (req, res, next) => {
       next(error);
     }
   } else {
-    return next(errorHandler(401, 'You can only view your own listings!'));
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
 
 export const getUser = async (req, res, next) => {
   try {
-    
     const user = await User.findById(req.params.id);
-  
-    if (!user) return next(errorHandler(404, 'User not found!'));
-  
+
+    if (!user) return next(errorHandler(404, "User not found!"));
+
     const { password: pass, ...rest } = user._doc;
-  
+
     res.status(200).json(rest);
   } catch (error) {
     next(error);
