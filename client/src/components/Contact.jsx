86ef4223ff302577b0510fb52bd4e52651ea4940
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2023-11-24 01:44:27 +0530

    Listing Page Completed

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const onChange = (e) => {
     setMessage(e.target.value);
   };
@@ -14,38 +17,51 @@ export default function Contact({ listing }) {
         const res = await fetch(`/api/user/${listing.userRef}`);
         const data = await res.json();
         setLandlord(data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
+        setError("Error fetching landlord information");
+        setLoading(false);
       }
     };
+
     fetchLandlord();
   }, [listing.userRef]);
+
   return (
     <>
-      {landlord && (
-        <div className='flex flex-col gap-2'>
-          <p>
-            Contact <span className='font-semibold'>{landlord.username}</span>{' '}
-            for{' '}
-            <span className='font-semibold'>{listing.name.toLowerCase()}</span>
-          </p>
-          <textarea
-            name='message'
-            id='message'
-            rows='2'
-            value={message}
-            onChange={onChange}
-            placeholder='Enter your message here...'
-            className='w-full border p-3 rounded-lg'
-          ></textarea>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        landlord && (
+          <div className="flex flex-col gap-2">
+            <p>
+              Contact <span className="font-semibold">{landlord.username}</span>{" "}
+              for{" "}
+              <span className="font-semibold">
+                {listing.name.toLowerCase()}
+              </span>
+            </p>
+            <textarea
+              name="message"
+              id="message"
+              rows="2"
+              value={message}
+              onChange={onChange}
+              placeholder="Enter your message here..."
+              className="w-full border p-3 rounded-lg"
+            ></textarea>
 
-          <Link
-          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
-          >
-            Send Message          
-          </Link>
-        </div>
+            <Link
+              to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+              className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+            >
+              Send Message
+            </Link>
+          </div>
+        )
       )}
     </>
   );
